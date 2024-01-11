5b054d1e4630d080600ec8cc92d782f39c8be4cc
ManhCuongN <nmcuong.cpr@gmail.com>
2024-01-11 11:58:39 +0700

    update order

--- src/configs/configUrl.js
+++ src/configs/configUrl.js
@@ -1,6 +1,7 @@
 const config = {
     development: {
       urlRabbitMQ: "amqps://ejaqrslc:FdbEYeWt40a6ggQ8zhoALDTREg1wcFUf@gerbil.rmq.cloudamqp.com/ejaqrslc",
+      urlProductService: "https://product-service-production-0ee8.up.railway.app/v1/api"
     }
    
   };
